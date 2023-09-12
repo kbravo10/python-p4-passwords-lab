76a17d5fade5eeeec318a13df7c5260bd3041c8c
Kevin Bravo <kbravo1024@gmail.com>
2023-09-12 16:22:56 -0700

    complete password lab

--- server/app.py
+++ server/app.py
@@ -21,23 +21,41 @@ class Signup(Resource):
         json = request.get_json()
         user = User(
             username=json['username'],
-            password_hash=json['password']
+            _password_hash=json['password']
         )
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if not session['user_id']:
+            return {},204
+        user = User.query.filter(User.id == session['user_id']).first().to_dict()
+        return user,200
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()
+        user_name = username['username']
+        
+        user = User.query.filter(User.username == user_name).first()
+        print(user.id)
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {}, 401
 
 class Logout(Resource):
-    pass
+    def delete(post):
+        session['user_id'] = None
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint = 'check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
